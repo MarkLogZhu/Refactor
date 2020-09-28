a1601eb04d4b6db8ad956c3b36eb9b24b8e1da2e
MarkLogZhu <markLogZhu@163.com>
2020-09-28 15:29:29 +0800

    章节一：发现 amountFor 函数的行为属于 Rental 对象，我们将其复制到 Rental 对象中去

--- src/main/java/com/learn/refactor/chapter1/refactor/Rental.java
+++ src/main/java/com/learn/refactor/chapter1/refactor/Rental.java
@@ -28,4 +28,27 @@ public class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+
+    private double amountFor(Rental rental) {
+        double result = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (rental.getDaysRented() > 2) {
+                    result += (rental.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NET_RELEASE:
+                result += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (rental.getDaysRented() > 3) {
+                    result += (rental.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
 }
